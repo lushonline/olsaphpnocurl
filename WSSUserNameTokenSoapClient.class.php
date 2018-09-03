f861a54f4632d0b35474d26cd1391995a10bcdb2
Martin Holden <martin.holden@skillsoft.com>
2018-09-03 14:30:25 +0100

    Updated to support PasswordText type, and passing user  info in options

--- WSSUserNameTokenSoapClient.class.php
+++ WSSUserNameTokenSoapClient.class.php
@@ -19,13 +19,14 @@ class WSSUserNameTokenSoapClient extends SoapClient{
 	const WSSE_PFX = 'wsse';
 	const WSU_NS   = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd';
 	const WSU_PFX  = 'wsu';
-	const PASSWORD_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest';
+	const PASSWORDTEXT_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText';
+	const PASSWORDDIGEST_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest';
 
 	//Private variables
 	private $username;
 	private $password;
-
-	private $proxy;
+	private $usepassworddigest;
+	private $expiry;
 	
 	/* ---------------------------------------------------------------------------------------------- */
 	/* Helper Functions                                                                               */
@@ -45,20 +46,20 @@ class WSSUserNameTokenSoapClient extends SoapClient{
 
 
 	private function generate_header() {
-
-		//Get the current time
-		$currentTime = time();
-		//Create the ISO8601 formatted timestamp
-		$timestamp=gmdate('Y-m-d\TH:i:s', $currentTime).'Z';
-		//Create the expiry timestamp 5 minutes later (60*5)
-		$expiretimestamp=gmdate('Y-m-d\TH:i:s', $currentTime + 300).'Z';
-		//Generate the random Nonce. The use of rand() may repeat the word if the server is very loaded.
-		$nonce=mt_rand();
-		//Create the PasswordDigest for the usernametoken
-		$passdigest=base64_encode(pack('H*',sha1(pack('H*',$nonce).pack('a*',$timestamp).pack('a*',$this->password))));
-
-		//Build the header text
-		$header='
+		if ($this->usepassworddigest) {
+			//Get the current time
+			$currentTime = time();
+			//Create the ISO8601 formatted timestamp
+			$timestamp=gmdate('Y-m-d\TH:i:s', $currentTime).'Z';
+			//Create the expiry timestamp 5 minutes later (60*5)
+			$expiretimestamp=gmdate('Y-m-d\TH:i:s', $currentTime + $this->expiry).'Z';
+			//Generate the random Nonce. The use of rand() may repeat the word if the server is very loaded.
+			$nonce=mt_rand();
+			//Create the PasswordDigest for the usernametoken
+			$passdigest=base64_encode(pack('H*',sha1(pack('H*',$nonce).pack('a*',$timestamp).pack('a*',$this->password))));
+			
+			
+			$header='
 			<wsse:Security env:mustUnderstand="1" xmlns:wsse="'.self::WSSE_NS.'" xmlns:wsu="'.self::WSU_NS.'">
 				<wsu:Timestamp wsu:Id="Timestamp-'.$this->guid().'">
 					<wsu:Created>'.$timestamp.'</wsu:Created>
@@ -66,12 +67,22 @@ class WSSUserNameTokenSoapClient extends SoapClient{
 				</wsu:Timestamp>
 				<wsse:UsernameToken xmlns:wsu="'.self::WSU_NS.'">
 					<wsse:Username>'.$this->username.'</wsse:Username>
-					<wsse:Password Type="'.self::PASSWORD_TYPE.'">'.$passdigest.'</wsse:Password>
+					<wsse:Password Type="'.self::PASSWORDDIGEST_TYPE.'">'.$passdigest.'</wsse:Password>
 					<wsse:Nonce>'.base64_encode(pack('H*',$nonce)).'</wsse:Nonce>
 					<wsu:Created>'.$timestamp.'</wsu:Created>
 				</wsse:UsernameToken>
 			</wsse:Security>
 			';
+		} else {
+			$header='
+			<wsse:Security env:mustUnderstand="1" xmlns:wsse="'.self::WSSE_NS.'" xmlns:wsu="'.self::WSU_NS.'">
+				<wsse:UsernameToken xmlns:wsu="'.self::WSU_NS.'">
+					<wsse:Username>'.$this->username.'</wsse:Username>
+					<wsse:Password Type="'.self::PASSWORDTEXT_TYPE.'">'.$this->password.'</wsse:Password>
+				</wsse:UsernameToken>
+			</wsse:Security>
+			';
+		}
 
 		$headerSoapVar=new SoapVar($header,XSD_ANYXML); //XSD_ANYXML (or 147) is the code to add xml directly into a SoapVar. Using other codes such as SOAP_ENC, it's really difficult to set the correct namespace for the variables, so the axis server rejects the xml.
 		$soapheader=new SoapHeader(self::WSSE_NS, "Security" , $headerSoapVar , true);
@@ -79,15 +90,63 @@ class WSSUserNameTokenSoapClient extends SoapClient{
 	}
 
 	/*It's necessary to call it if you want to set a different user and password*/
-	public function __setUsernameToken($username,$password){
+	public function __setUsernameToken($username,$password,$usepassworddigest=true,$expiry=300){
 		$this->username=$username;
 		$this->password=$password;
+		
+		if(is_bool($usepassworddigest)) {
+			$this->usepassworddigest=$usepassworddigest;
+		} else {
+			$this->usepassworddigest=true;
+		}
+		
+		if (is_int($expiry)) {
+			$this->expiry=$expiry;
+		} else 
+		{
+			$this->expiry=300;
+		}
 	}
 
 	public function __getLastRequestHeaders() {
 		return implode("\n", $this->__last_request_headers)."\n";
 	}
 
+	public function __construct($endpoint,$options) {
+		$this->username="";
+		$this->password="";
+		$this->usepassworddigest=true;
+		$this->expiry=300;
+		
+		if (array_key_exists("wsse_username",$options))
+		{
+			$this->username=$options["wsse_username"];
+		}
+		
+		if (array_key_exists("wsse_password",$options))
+		{
+			$this->password=$options["wsse_password"];
+		}
+
+		if (array_key_exists("wsse_usepassworddigest",$options))
+		{
+			if(is_bool($options["wsse_usepassworddigest"])) {
+				$this->password=$options["wsse_usepassworddigest"];
+			}
+		}
+		
+		if (array_key_exists("wsse_expiry",$options))
+		{
+			if(is_int($options["wsse_expiry"])) {
+				$this->expiry=$options["wsse_expiry"];
+			}
+		}
+
+		$result = parent::__construct($endpoint, $options);
+		return $result;
+	}
+	
+	
 	/*Overload the original method, and add the WS-Security Header */
 	public function __soapCall($function_name,$arguments,$options=array(),$input_headers=null,&$output_headers=array()){
 		$result = parent::__soapCall($function_name,$arguments,$options,$this->generate_header());
